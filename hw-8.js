6801a0b670f81d02d23c22b8dedcb0fc5659406f
Viktoriia Lozenko <Lozenkoviktoriia@gmail.com>
2021-04-22 22:21:52 +0300

    push

--- hw-8.js
+++ hw-8.js
@@ -1,1 +1,42 @@
-import gallery from './gallery-items.js';
+import galleryImages from './gallery-items.js';
+
+const gallery = document.querySelector('.js-gallery');
+const cardsMarkup = createGalleryImages(galleryImages);
+gallery.addEventListener('click', galleryClick);
+
+
+function galleryClick(e) {
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+    const lightboxEl = e.target;
+    lightboxEl.classList.add("is-active");
+    console.log(e.target);
+};
+
+
+
+function createGalleryImages(galleryImages) {
+    return galleryImages.map(({ preview, original, description }) => {
+        return ` 
+        <li class="gallery__item">
+            <a
+            class="gallery__link"
+            href="#"
+        >
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+            </a>
+        </li>
+        `;
+    })
+        .join('');
+}
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+
